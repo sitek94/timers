8c33894b9f6cb5d14486c3dfc222d222e2aaa0de
Maciek <msitkowski94@gmail.com>
2022-04-28 18:21:31 +0200

    add dummy buttons with icons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // Layout
+
+  const timers = [
+    { label: 'Spanish', icon: '🇪🇸' },
+    { label: 'Reading', icon: '📖' },
+    { label: 'Vlog', icon: '🎥' },
+    { label: 'Stretching', icon: '🧘‍♀️' },
+    { label: 'Journal', icon: '📝' },
+    { label: 'Charisma', icon: '🗣' },
+  ]
+</script>
+
+<main>
+  {#each timers as { label, icon }}
+    <button on:click={() => alert(label)}>
+      <span role="img" aria-label={label}>
+        {icon}
+      </span>
+    </button>
+  {/each}
+</main>
+
+<style>
+  :global(body) {
+    color: #fff;
+    background: #222;
+    padding: 2rem;
+  }
+
+  main {
+    display: flex;
+    gap: 1rem;
+  }
+
+  button {
+    font-size: 3rem;
+    border: 3px solid #fff;
+    border-radius: 1rem;
+    cursor: pointer;
+    background: #fff;
+  }
+  button:hover {
+    border-color: cornflowerblue;
+  }
+</style>
